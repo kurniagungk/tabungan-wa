6f53148228129565b7feffa43ef4e1ab09a08245
kurniagungk <37070156+kurniagungk@users.noreply.github.com>
2021-09-25 18:16:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
 const client = new Client({
     restartOnAuthFail: true,
     puppeteer: {
-        headless: false,
+        headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
